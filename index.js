017b649afa3d61238c072639c830dfeb2ff298db
Hathaway-Shan <hathaway.shan@gmail.com>
2022-11-06 12:35:49 -0800

    we have working display of scraped json data

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
 const puppeteer = require("puppeteer");
+const express = require("express");
+const app = express();
+const PORT = 8000;
 
-(async () => {
-  /* Initiate the Puppeteer browser */
-  // check with { headless : false } arg
-  const browser = await puppeteer.launch();
+app.get("/", function (req, res) {
+  res.json("web scraper test home");
+});
 
-  // create a newPage instance to have access to page methods
-  const page = await browser.newPage();
+app.get("/mons", function (req, res) {
+  //this is a self running async function, hence the weird syntax
+  (async () => {
+    /* Initiate the Puppeteer browser */
+    // check with { headless : false } arg
+    const browser = await puppeteer.launch();
 
-  // we cannot console log in the browser instance without this code
-  // check element values in the console instance using headless
-  page.on("console", async (e) => {
-    const args = await Promise.all(e.args().map((a) => a.jsonValue()));
-    console[e.type() === "warning" ? "warn" : e.type()](...args);
-  });
+    // create a newPage instance to have access to page methods
+    const page = await browser.newPage();
 
-  //now we can navigate to our intended page
-  await page.goto("https://pokedex.org/", { waitUntil: "networkidle0" });
-  //call evaluate method to select specific information from the page
+    // we cannot console log in the browser instance without this code
+    // check element values in the console instance using headless
+    page.on("console", async (e) => {
+      const args = await Promise.all(e.args().map((a) => a.jsonValue()));
+      console[e.type() === "warning" ? "warn" : e.type()](...args);
+    });
 
-  const data = await page.evaluate(() => {
-    const mons = document.querySelectorAll("#monsters-list li span");
-    const arr = [];
+    //now we can navigate to our intended page
+    await page.goto("https://pokedex.org/", { waitUntil: "networkidle0" });
+    //call evaluate method to select specific information from the page
 
-    for (i = 0; i < mons.length; i++) {
-      arr.push({
-        name: mons[i].innerText,
-      });
-    }
-    return arr;
-  });
-  console.log("data is: ", data);
-  await browser.close();
-})();
+    const data = await page.evaluate(() => {
+      const mons = document.querySelectorAll("#monsters-list li span");
+      const arr = [];
+
+      for (i = 0; i < mons.length; i++) {
+        arr.push({
+          name: mons[i].innerText,
+        });
+      }
+      return arr;
+    });
+    // console.log("data is: ", data); //returns expected list of pokemon names
+    await browser.close();
+    res.json(data);
+  })();
+});
+
+//the localhost 8000 breaks without this line
+//check with: app.listen(8000, "0.0.0.0", () => console.log('listening on port 8000'));
+app.listen(8000, "0.0.0.0"); //returns expected
